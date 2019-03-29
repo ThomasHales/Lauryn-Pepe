983ced99e081811d53eaa6af638c5217e40bb409
CalebUddyback <48996578+CalebUddyback@users.noreply.github.com>
2019-03-29 13:32:37 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -4,6 +4,8 @@ $servername = "localhost";
 $username = "halest_Lauryn-Pepe";
 $password = "meme123";
 $db_name = "halest_Lauryn-Pepe";
+$formErr = array("Invalid User","Invalid Password Format","Invalid Username Format");
+$E = 0;
 
 $conn = new mysqli($servername, $username, $password, $db_name);
 
@@ -15,12 +17,20 @@ $uname=$_POST["username"];
 
 if(!$uname){
     echo "Uname was not in the form\n"; 
+} else{
+		if(!preg_match('/^[a-zA-Z0-9_-]+$/i', $uname)) {
+			$E=2;
+    } 
 }
 
 $pass=$_POST["password"]; 
 
 if(!$pass){
     echo "Pass was not in the form\n"; 
+} else{
+		if(!preg_match('/^[a-zA-Z0-9_-]+$/i', $pass)) {
+			$E++;
+    } 
 }
 
 $sql = "SELECT Username, Password FROM Users"; 
@@ -29,9 +39,15 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         if($uname == $row["Username"] && $pass == $row["Password"]){
-            echo $uname. " Signed in! <br>"; 
+            echo "Signed In As: $uname <br>"; 
         }else{
-            echo "Invalid User";
+			if ($E >= 2){
+				echo $formErr[2];
+			} else if ($E == 1){
+				echo $formErr[1];
+			} else if ($E == 0){
+				echo $formErr[0];
+			}
         }
     }
 } else {
