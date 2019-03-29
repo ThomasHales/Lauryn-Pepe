9bc6eb2543439231fdd171419a4ce905a32d759f
CalebUddyback <48996578+CalebUddyback@users.noreply.github.com>
2019-03-29 17:24:28 -0400

    Update login.php

--- login.php
+++ login.php
@@ -18,8 +18,8 @@ $uname=$_POST["username"];
 if(!$uname){
     echo "Uname was not in the form\n"; 
 } else{
-		if(!preg_match('/^[a-zA-Z0-9_-]+$/i', $uname)) {
-			$E=2;
+    if(!preg_match('/^[a-zA-Z0-9_-]+$/i', $uname)) {
+        $E=2;
     } 
 }
 
@@ -28,8 +28,8 @@ $pass=$_POST["password"];
 if(!$pass){
     echo "Pass was not in the form\n"; 
 } else{
-		if(!preg_match('/^[a-zA-Z0-9_-]+$/i', $pass)) {
-			$E++;
+    if(!preg_match('/^[a-zA-Z0-9_-]+$/i', $pass)) {
+        $E++;
     } 
 }
 
@@ -41,13 +41,13 @@ if ($result->num_rows > 0) {
         if($uname == $row["Username"] && $pass == $row["Password"]){
             echo "Signed In As: $uname <br>"; 
         }else{
-			if ($E >= 2){
-				echo $formErr[2];
-			} else if ($E == 1){
-				echo $formErr[1];
-			} else if ($E == 0){
-				echo $formErr[0];
-			}
+	    if ($E >= 2){
+	        echo $formErr[2];
+	    } else if ($E == 1){
+	        echo $formErr[1];
+	    } else if ($E == 0){
+	        echo $formErr[0];
+	    }
         }
     }
 } else {
